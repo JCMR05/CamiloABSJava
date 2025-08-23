a6ed129c37b9e00215612128b845eb907dffedcd
JCMR05 <juancamilomorarangel@gmail.com>
2025-08-23 12:20:51 -0500

    formulario usuario, modal delete y action

--- sistema/routes/web.php
+++ sistema/routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('plantilla.app');
+    return view('welcome');
+});
+
+Route::get('/app', function () {
+    return view('usuario.index');
+});
+
+Route::get('/action', function () {
+    return view('usuario.action');
 });
